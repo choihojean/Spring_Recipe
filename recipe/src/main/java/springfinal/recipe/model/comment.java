c03ac5ca411b1c05c00baefac55af6e319c0b1a5
choihojean <chlghwls1231@naver.com>
2024-11-14 15:36:01 +0900

    refactor: Comment 수정

--- recipe/src/main/java/springfinal/recipe/model/comment.java
+++ recipe/src/main/java/springfinal/recipe/model/comment.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Getter @Builder
 @Entity
 @Table(name="comment")
-public class comment {
+public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 인덱스

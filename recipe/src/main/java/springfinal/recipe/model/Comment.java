195937997aa467c91e884efae847127a50eb1970
Ourumo <ss8638@naver.com>
2024-11-20 17:25:41 +0900

    feat: Comment 엔티티 구조

--- recipe/src/main/java/springfinal/recipe/model/Comment.java
+++ recipe/src/main/java/springfinal/recipe/model/Comment.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor @AllArgsConstructor
@@ -17,13 +18,19 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 인덱스
 
-    @ManyToOne
-    @JoinColumn(name="recipe_id")
-    private Recipe recipe; // 레시피 인덱스
-
     private String userNickname; // 유저 닉네임
     private String content; // 내용
-    private Integer parentCommentId; // 부모 댓글
-    private List<Integer> childCommentsId; //자식 댓글
+
+    @ManyToOne
+    @JoinColumn(name = "recipe_id")
+    private Recipe recipe; // 레시피 조인
+
+    @ManyToOne
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parentComment; // 부모 댓글 조인
+
+    @OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> childComments = new ArrayList<>(); // 자식 댓글들
+
     private Boolean is_deleted; // 삭제 여부
 }

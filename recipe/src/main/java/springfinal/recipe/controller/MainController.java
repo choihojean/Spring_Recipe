4e15abdcc4ce3b111fc98edbb5ead46f1eb0ea5d
Ourumo <ss8638@naver.com>
2024-11-21 09:59:50 +0900

    feat: 컨트롤러 분리(MainController)

---
+++ recipe/src/main/java/springfinal/recipe/controller/MainController.java
@@ -0,0 +1,15 @@
+package springfinal.recipe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class MainController {
+    @GetMapping("/main") // 메인 페이지
+    public String mainPage() {
+        return "recipe";
+    }
+}

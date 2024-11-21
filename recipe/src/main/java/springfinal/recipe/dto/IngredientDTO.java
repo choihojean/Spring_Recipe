4c9924bb1f47bc797974be660a8119da8b325607
Ourumo <ss8638@naver.com>
2024-11-21 09:37:08 +0900

    remove: 불필요한 임포트 삭제

--- recipe/src/main/java/springfinal/recipe/dto/IngredientDTO.java
+++ recipe/src/main/java/springfinal/recipe/dto/IngredientDTO.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import springfinal.recipe.model.RecipeIngredient;
 
 import java.util.List;
 

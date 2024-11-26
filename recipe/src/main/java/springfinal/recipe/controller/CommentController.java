425f3151b8b1307edc780275a3d5f24b66aee2c7
Ourumo <ss8638@naver.com>
2024-11-26 13:33:49 +0900

    refactor: 파라미터 받는 방식 변경

--- recipe/src/main/java/springfinal/recipe/controller/CommentController.java
+++ recipe/src/main/java/springfinal/recipe/controller/CommentController.java
@@ -3,6 +3,7 @@ package springfinal.recipe.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import springfinal.recipe.dto.CommentDTO;
@@ -16,8 +17,8 @@ public class CommentController {
     @Autowired
     private CommentService commentService;
 
-    @GetMapping("/api/comment")
-    public List<CommentDTO> readRecipeComment(@RequestParam("id") Long recipeId) {
+    @GetMapping("/api/comment/{id}")
+    public List<CommentDTO> readRecipeComment(@PathVariable("id") Long recipeId) {
         return commentService.findByRecipeId(recipeId);
     }
 }
